71b80838e83a493994d0e37dc50ed232056632cf
Aryan Sharma <aryandz0529@gmail.com>
2025-03-25 14:48:43 +0530

    first commit

---
+++ 01basic.js
@@ -0,0 +1,23 @@
+
+const account_id = 1225
+//let assign it to the memory
+let Emailid = "aryandz0529@gmail.com"
+
+//we do not use var becouse if it is in if { } block and a person create a integer name pass and there is outside if block also then it will change all password value
+var Password = "70223"
+let account_state 
+
+/*
+prefer not to use var as it have problem in block scope and functional scope
+*/
+accountCity="jaipur"
+
+console.log(Emailid);
+
+console.table([Emailid,Password,accountCity,account_state])
+
+
+
+
+const paglu = "aryan"
+console.log(paglu)
---
+++ datatype.js
@@ -0,0 +1,18 @@
+"use strict";       // treate all js  code to new version 
+
+let s_name = "aryan"
+let age =21
+let cgpa = 9.2
+let status=true
+let balance ;                    //  => it will give undefined a answer as i  do not give a value
+let temp = null // setting it to null    use in case of temp while showing error 
+
+//console.log(balance)
+//console.log(typeof(age));
+
+let test = Number(s_name)
+console.log((typeof (test)))         //=> it will give NaN  . not a number    
+
+//"33" => 33 Number
+//"33abc" => NAN 
+
